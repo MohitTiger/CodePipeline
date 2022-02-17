495c0f925cbfe75711ecad2d78f685c1e327f561
MohitTiger <97945069+MohitTiger@users.noreply.github.com>
2022-02-17 17:11:55 +0530

    Update Dependencies_emr.py

--- airflowcode/Dependencies_emr.py
+++ airflowcode/Dependencies_emr.py
@@ -70,7 +70,7 @@ def get_cluster_dns(cluster_id):
 def wait_for_cluster_creation(cluster_id):
     emr.get_waiter('cluster_running').wait(ClusterId=cluster_id)
 
-#Livy
+#function for Livy
 def livy_task(master_dns):
     # 8998 is the port on which the Livy server runs
     host = 'http://' + master_dns + ':8998'
